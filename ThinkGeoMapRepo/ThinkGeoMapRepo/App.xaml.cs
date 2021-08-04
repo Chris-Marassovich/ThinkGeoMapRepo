05c551b6ecbc3c22f441f2317eb522a3eaa3f53e
Chris Marrasovich <chris.marassovich@distillerysoftware.com>
2021-08-05 09:27:23 +1000

    Added FontAwesome.

--- ThinkGeoMapRepo/ThinkGeoMapRepo/App.xaml.cs
+++ ThinkGeoMapRepo/ThinkGeoMapRepo/App.xaml.cs
@@ -4,6 +4,10 @@ using ThinkGeoMapRepo.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = "FA-R")]
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = "FA-S")]
+[assembly: ExportFont("Font Awesome 5 Brands-Regular-400.otf", Alias = "FA-B")]
+
 namespace ThinkGeoMapRepo
 {
     public partial class App : Application
